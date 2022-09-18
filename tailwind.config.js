332f217355774cd92671025fb7d2f43fa1f412f7
Bilal Arve <bilalarve@gmail.com>
2022-09-18 17:50:36 +0700

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6B00",
+        dark: "#1C1C1C",
+      },
+    },
+  },
+  plugins: [],
+};
